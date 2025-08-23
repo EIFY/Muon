bbe27435ad1ae1d02a06e4f5aae8b3ea575052cf
EIFY <chuanchih@gmail.com>
2025-08-22 22:01:08 -0700

    intended dist_test (untested)
    for https://github.com/KellerJordan/Muon/issues/46

--- dist_test.py
+++ dist_test.py
@@ -1,14 +1,11 @@
+import os
+
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn as nn
 
-from muon import SingleDeviceMuon
-
-dim = 128
-
-model = nn.Sequential(*[nn.Linear(dim, dim, bias=False) for _ in range(8)])
-cross_entropy = nn.CrossEntropyLoss()
+from muon import Muon
 
 def batch(size, dim):
     data = torch.normal(mean=0, std=1, size=(size, dim))
@@ -20,12 +17,69 @@ def print_momentum_buffer(optimizer):
     for parameter_id, s in optimizer.state_dict()['state'].items():
         print(f'{parameter_id}:', f'{s['momentum_buffer'].shape} tensor' if 'momentum_buffer' in s else None)
 
-optimizer = SingleDeviceMuon(model.parameters())
+def is_primary(rank):
+    return not rank
 
-data, target = batch(1, dim)
-loss = cross_entropy(model(data), target)
-loss.backward()
-optimizer.step()
-optimizer.zero_grad()
+def main():
 
-print_momentum_buffer(optimizer)
+    world_size = int(os.environ.get("WORLD_SIZE", 1))
+    ngpus_per_node = torch.cuda.device_count()
+    world_size *= ngpus_per_node
+
+    mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, world_size))
+
+def main_worker(gpu, ngpus_per_node, world_size):
+
+    backend = 'nccl'
+    dist_url = 'env://'
+    rank = int(os.environ["RANK"])
+    rank = rank * ngpus_per_node + gpu
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(rank)
+
+    dist.init_process_group(
+        backend=backend,
+        init_method=dist_url,
+        world_size=world_size,
+        rank=rank
+    )
+
+    dim = 128
+    model = nn.Sequential(*[nn.Linear(dim, dim, bias=False) for _ in range(world_size)])
+    cross_entropy = nn.CrossEntropyLoss()
+    model.cuda()
+    cross_entropy.cuda()
+
+    model = torch.nn.parallel.DistributedDataParallel(model)
+
+    optimizer = Muon(model.parameters())
+
+    data, target = batch(1, dim)
+    loss = cross_entropy(model(data), target)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    if is_primary(rank):
+        print_momentum_buffer(optimizer)
+
+    def gather(optimizer):
+        for group in optimizer.param_groups:
+            params = group["params"]
+            buffers = [optimizer.state[p].get("momentum_buffer", torch.zeros_like(p)) for p in params]
+            buffers.extend([torch.empty_like(params[-1])] * (world_size - len(params) % world_size))
+            for base_i in range(len(params))[::world_size]:
+                dist.gather(buffers[base_i + rank], gather_list=buffers[base_i:base_i + world_size])
+
+    optimizer.register_state_dict_pre_hook(gather)
+
+    data, target = batch(1, dim)
+    loss = cross_entropy(model(data), target)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    if is_primary(rank):
+        print_momentum_buffer(optimizer)
+
+if __name__ == '__main__':
+    main()
